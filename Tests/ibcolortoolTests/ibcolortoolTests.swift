8326e9b7a16311705e05218c3e3895305497cbf5
Mattt <mattt@me.com>
2020-05-23 11:04:57 -0700

    Add test for object id output

--- Tests/ibcolortoolTests/ibcolortoolTests.swift
+++ Tests/ibcolortoolTests/ibcolortoolTests.swift
@@ -19,28 +19,34 @@ final class IBColorToolTests: XCTestCase {
     }
 
     func testUIColorDeclarationFormatStoryboard() throws {
-         let storyboardFilePath = (try temporaryFile(contents: storyboard, extension: "storyboard")).path
-         let actual = try runIBColorTool(with: [storyboardFilePath])
-         let expected = "UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)\n"
+        let storyboardFilePath = (try temporaryFile(contents: storyboard, extension: "storyboard")).path
+        let actual = try runIBColorTool(with: [storyboardFilePath])
+        let expected = "UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)\n"
 
-         XCTAssertEqual(actual, expected)
-     }
+        XCTAssertEqual(actual, expected)
+    }
 
-     func testRGBHexFormatStoryboard() throws {
-         let storyboardFilePath = (try temporaryFile(contents: storyboard, extension: "storyboard")).path
-         let actual = try runIBColorTool(with: ["--format", "hex", storyboardFilePath])
-         let expected = "#00FF00\n"
+    func testRGBHexFormatStoryboard() throws {
+        let storyboardFilePath = (try temporaryFile(contents: storyboard, extension: "storyboard")).path
+        let actual = try runIBColorTool(with: ["--format", "hex", storyboardFilePath])
+        let expected = "#00FF00\n"
 
-         XCTAssertEqual(actual, expected)
-     }
+        XCTAssertEqual(actual, expected)
+    }
 
-    func testStoryboardAndXIB() throws {
+    func testStoryboardAndXIBWithObjectID() throws {
         let storyboardFilePath = (try temporaryFile(contents: storyboard, extension: "storyboard")).path
         let xibFilePath = (try temporaryFile(contents: xib, extension: "xib")).path
-        let actual = try runIBColorTool(with: ["--format", "hex", storyboardFilePath, xibFilePath])
+        let actual = try runIBColorTool(with: [
+                            "--format", "hex",
+                            "--include-object-id",
+                            storyboardFilePath,
+                            xibFilePath
+                         ])
+
         let expected = """
-        #00FF00
-        #FF9200
+        3Dd-xk-CM4\t#00FF00
+        iN0-l3-epB\t#FF9200
 
         """
 
